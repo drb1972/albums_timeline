6a62e2070c8f62960002d95774e59dc91766f56a
Diego Rodriguez <diego.rodriguez@broadcom.com>
2025-03-31 16:07:19 +0200

    c1

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ st.set_page_config(page_title="Bands Timeline",
                    layout="wide",
                    initial_sidebar_state="expanded")
 
+hide_streamlit_style = """
+            <style>
+                /* Hide the Streamlit header and menu */
+                header {visibility: hidden;}
+                /* Optionally, hide the footer */
+                .streamlit-footer {display: none;}
+                /* Hide your specific div class, replace class name with the one you identified */
+                .st-emotion-cache-uf99v8 {display: none;}
+            </style>
+            """
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
 #------------------- SESSION STATE ------------------------------------------
 if 'selected_bands_list' not in st.session_state:
     st.session_state.selected_bands_list = []
