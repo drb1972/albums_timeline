54edf5182d1b82f6fec4184443e7596edd9b6af1
Diego Rodriguez <diego.rodriguez@broadcom.com>
2025-03-31 16:14:07 +0200

    c1

--- main.py
+++ main.py
@@ -10,19 +10,30 @@ st.set_page_config(page_title="Bands Timeline",
                    layout="wide",
                    initial_sidebar_state="expanded")
 
-hide_streamlit_style = """
-            <style>
-                /* Hide the Streamlit header and menu */
-                header {visibility: hidden;}
-                /* Optionally, hide the footer */
-                .streamlit-footer {display: none;}
-                /* Hide your specific div class, replace class name with the one you identified */
-                .st-emotion-cache-uf99v8 {display: none;}
-            </style>
-            """
+# hide_streamlit_style = """
+#             <style>
+#                 /* Hide the Streamlit header and menu */
+#                 header {visibility: hidden;}
+#                 /* Optionally, hide the footer */
+#                 .streamlit-footer {display: none;}
+#                 /* Hide your specific div class, replace class name with the one you identified */
+#                 .st-emotion-cache-uf99v8 {display: none;}
+#             </style>
+#             """
 
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+
+#----------------------Hide Streamlit footer----------------------------
+hide_st_style = """
+
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
+#--------------------------------------------------------------------
 
 #------------------- SESSION STATE ------------------------------------------
 if 'selected_bands_list' not in st.session_state:
